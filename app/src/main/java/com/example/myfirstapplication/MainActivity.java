5e121e90a5c8fe82262ce88cd148e6c4142a9337
david.giczi@gmail.com <david.giczi@gmail.com>
2025-02-22 13:06:16 +0100

    intersectionOption

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.Gravity;
 import android.view.View;
 
 import androidx.core.view.WindowCompat;
@@ -18,6 +21,11 @@ import com.example.myfirstapplication.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.PopupWindow;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +44,39 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+    private void exitAppDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle(R.string.close_app_title);
+        builder.setMessage(R.string.close_app_question);
+
+        builder.setPositiveButton(R.string.yes, (dialog, which) -> {
+            System.exit(0);
+            dialog.dismiss();
+        });
+
+        builder.setNegativeButton(R.string.no, (dialog, which) -> dialog.dismiss());
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void popupStartGameDialog(){
+        ViewGroup container = (ViewGroup) getLayoutInflater().inflate(R.layout.fragment_first, null);
+        PopupWindow startGameWindow = new PopupWindow(container, 1000, 700, true);
+        startGameWindow.showAtLocation(binding.getRoot(), Gravity.CENTER, 0, - 400);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
